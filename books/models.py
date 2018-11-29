314be92e054c6f959dd35841540c3e4ba6067fee
Alex Brooks <alexanderathome@btinternet.com>
2018-11-29 14:21:53 +0000

    new database

--- books/models.py
+++ books/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    item = models.CharField(max_length=250)
-    price = models.CharField(max_length=250)
-    description = models.CharField(max_length=250)
-    image = models.CharField(max_length=250)
-    #ID = models.CharField(max_length=10)
+    item = models.CharField(max_length=250, default="item")
+    price = models.CharField(max_length=250, default="price")
+    description = models.CharField(max_length=250, default="description")
+    image = models.CharField(max_length=250, default="image")
+    ID = models.CharField(max_length=4, default="0000")
 
 def __string__(self):
     return self.title
