e20012799659864f602f516bd132c98a7930ad09
Alex Brooks <alexanderathome@btinternet.com>
2018-12-07 14:12:28 +0000

    new superuser created

--- books/tests.py
+++ books/tests.py
@@ -3,51 +3,31 @@ from .models import Book
 
 # Create your tests here.
 class TestBasic2(unittest.TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Book.objects.create(item = "Test item", price = "10", description = "test description", image = "image link", ID="9999")
 
-    def setUp(self):
-        self.a = 1
+    def test_Item(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field('item').verbose_name
+        self.assertEquals(field_label, 'item')
 
-    def tearDown(self):
-        del self.a
+    def test_Price(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field('price').verbose_name
+        self.assertEquals(field_label, 'price')
 
-    def test_basic1(self):
-        "Basic with setup"
+    def test_Description(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field('description').verbose_name
+        self.assertEquals(field_label, "description")
 
-        self.assertNotEqual(self.a, 2)
+    def test_Image(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field("image").verbose_name
+        self.assertEquals(field_label, "image")
 
-    def test_basic2(self):
-        "Basic2 with setup"
-        assert self.a != 2
-
-    def test_fail(self):
-        "This test should fail"
-        assert self.a == 2
-    # @classmethod
-    # def setUpTestData(cls):
-    #     Book.objects.create(item = "Test item", price = "10", description = "test description", image = "image link", ID="9999")
-    #
-    # def test_Item(self):
-    #     book = Book.objects.get(id=1)
-    #     field_label = book._meta.get_field('item').verbose_name
-    #     self.assertEquals(field_label, 'item')
-    #
-    # def test_Price(self):
-    #     book = Book.objects.get(id=1)
-    #     field_label = book._meta.get_field('price').verbose_name
-    #     self.assertEquals(field_label, 'price')
-    #
-    # def test_Description(self):
-    #     book = Book.objects.get(id=1)
-    #     field_label = book._meta.get_field('description').verbose_name
-    #     self.assertEquals(field_label, "description")
-    #
-    # def test_Image(self):
-    #     book = Book.objects.get(id=1)
-    #     field_label = book._meta.get_field("image").verbose_name
-    #     self.assertEquals(field_label, "image")
-    #
-    # def test_ID(self):
-    #     book = Book.objects.get(id=1)
-    #     field_label = book._meta.get_field("ID").verbose_name
-    #     self.assertEquals(field_label, "ID")
-    #
+    def test_ID(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field("ID").verbose_name
+        self.assertEquals(field_label, "ID")
