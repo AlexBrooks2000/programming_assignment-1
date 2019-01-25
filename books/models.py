10372e1a2d4dc96893baeb63ebc8c5c0ef62b61d
Alex Brooks <alexanderathome@btinternet.com>
2019-01-25 11:25:44 +0000

    polymorphism added

--- books/models.py
+++ books/models.py
@@ -9,8 +9,18 @@ class Book(models.Model):
     image = models.CharField(max_length=250, default="image")
     product_ID = models.CharField(max_length=4, default="0000")
 
+    def VAT(self, price):
+        int_price = int(price)
+        vat = int_price*0.15
+        return vat
+
 class Shoe(Book):
     shoe_size = models.CharField(max_length=2, default="00")
+
+    def VAT(self, price):
+        int_price = int(price)
+        vat = int_price*0.20
+        return vat
 
 def __string__(self):
     return self.item
