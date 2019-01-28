c39b246db9efe027bd5f4613eb648c8ab167ab8c
Alex Brooks <alexanderathome@btinternet.com>
2019-01-28 22:00:00 +0000

    comments added

--- books/models.py
+++ books/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 # Create your models here.
-#model for item, example of encapulation
-
+#model for item,
 
 class Book(models.Model):
 
@@ -12,15 +11,16 @@ class Book(models.Model):
     image = models.CharField(max_length=250, default="image")
     product_ID = models.CharField(max_length=4, default="0000")
     vat = models.FloatField(editable=False, default=0)
-    def save(self, *args, **kwargs):
+
+    def save(self, *args, **kwargs): #function calculates VAT of item
         vat = self.price * 0.15
         super(Book, self).save(*args, **kwargs)
         return vat
 
-class Shoe(Book): #Subclass of Book, example of inheritence
+class Shoe(Book): #Subclass of Book
     shoe_size = models.CharField(max_length=2, default="00")
 
-    def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs): #Claculates VAT of shoe as VAT is diferent to book
         vat = self.price * 0.2
         super(Book, self).save(*args, **kwargs)
         return vat
